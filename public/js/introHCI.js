20d9968257bf175eb2ee02968bcecb217f63366a
Silvia Chyou <schyou@ucsd.edu>
2016-02-18 07:25:26 +0000

    Added Timing

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -1,4 +1,5 @@
 'use strict';
+var startTime;
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
@@ -10,11 +11,13 @@ $(document).ready(function() {
  */
 function initializePage() {
 	// your code here
+	startTime = new Date().getTime();
 	$('.likeBtn').click(likeClick);
 }
 
 function likeClick(e){
 	e.preventDefault();
+	var endTime = new Date().getTime();
 	ga("send", "event", "like", "click");
-
+	ga('send', 'timing', 'time_spent', 'clicklike', endTime-startTime);
 }
