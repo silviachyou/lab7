3b4c506fe5c054b577af5d21b7e955f08a92e411
Silvia Chyou <schyou@ucsd.edu>
2016-02-18 07:17:21 +0000

    Added Experiment Header

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,17 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(req, res){
-		projects['grid'] = false;
-  	res.render('index', projects);
+		//projects['grid'] = false;
+		var random_num = Math.random();
+		console.log(random_num);
+
+		if (random_num >= 0) {
+		  projects['grid'] = false;
+		  res.render('index', projects);
+		} else {
+		  res.redirect('/grid');
+		}
+  	//res.render('index', projects);
 };
 
 exports.viewGrid = function(req, res){
